3def7138adeee925519e12b1c59aeb80e507ac05
Anastasiya Yakovleva <dovakot@yandex.ru>
2021-02-22 18:01:22 +0800

    Добавляет логику обработки пользовательского ввода

--- js/form.js
+++ js/form.js
@@ -1,8 +1,20 @@
 const DIGITS = 5;
 
+const priceList = {
+  bungalow: 0,
+  flat: 1000,
+  house: 5000,
+  palace: 10000,
+};
+
 const adForm = document.querySelector('.ad-form');
 const fieldsetList = adForm.children;
+
 const fieldAddress = adForm.querySelector('#address');
+const fieldPrice = adForm.querySelector('#price');
+const fieldType = adForm.querySelector('#type');
+const fieldTimein = adForm.querySelector('#timein');
+const fieldTimeout = adForm.querySelector('#timeout');
 
 const disableAdForm = () => {
   adForm.classList.add('ad-form--disabled');
@@ -23,6 +35,21 @@ const enableAdForm = () => {
 const setAddress = ({lat, lng}) => {
   fieldAddress.value = `${lat.toFixed(DIGITS)}, ${lng.toFixed(DIGITS)}`;
 };
+
+fieldType.addEventListener('change', () => {
+  const price = priceList[fieldType.value];
+
+  fieldPrice.min = price;
+  fieldPrice.placeholder = price;
+});
+
+fieldTimein.addEventListener('change', () => {
+  fieldTimeout.value = fieldTimein.value;
+});
+
+fieldTimeout.addEventListener('change', () => {
+  fieldTimein.value = fieldTimeout.value;
+});
 
 disableAdForm();
 
